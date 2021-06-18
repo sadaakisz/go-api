d6ba57e1d18eab1c55714f740a23a4da450e8bf8
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-06-18 16:33:03 -0500

    Updated with 5 columns, id and cluster.

--- model/model.go
+++ model/model.go
@@ -1,6 +1,11 @@
 package model
 
 type Ciudadano struct {
-	ESTRATO_SOCIOECONOMICO int `json:"estrato_socioeconomico"`
-	SEGURIDAD_NOCTURNA     int `json:"seguridad_nocturna"`
+	ID                     int     `json:"id"`
+	ESTRATO_SOCIOECONOMICO float32 `json:"estrato_socioeconomico"`
+	SEGURIDAD_NOCTURNA     float32 `json:"seguridad_nocturna"`
+	GRUPOS_EDAD            float32 `json:"grupos_edad"`
+	CONFIANZA_POLICIA      float32 `json:"confianza_policia"`
+	PRONTO_DELITO          float32 `json:"pronto_delito"`
+	CLUSTER                int     `json:"cluster"`
 }
